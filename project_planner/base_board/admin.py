f260620a4476573c907370cded0f22cc85c0c209
rajesh196rsh <rajesh196rsh@gmail.com>
2024-02-25 22:27:17 +0530

    models added

--- project_planner/base_board/admin.py
+++ project_planner/base_board/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Team, TeamUsersMapping, Task, ProjectBoard
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Team)
+admin.site.register(TeamUsersMapping)
+admin.site.register(Task)
+admin.site.register(ProjectBoard)
--- project_planner/base_board/models.py
+++ project_planner/base_board/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    display_name = models.CharField(max_length=64)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.CharField(max_length=128)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class TeamUsersMapping(models.Model):
+    team_id = models.IntegerField()
+    user_id = models.IntegerField()
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Mets:
+        unique_together = ('team_id', 'user_id')
+
+
+task_status_choices = (
+    ('1', 'OPEN'),
+    ('2', 'IN_PROGRESS'),
+    ('3', 'COMPLETE')
+)
+
+class Task(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=128)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    board = models.IntegerField()
+    status = models.CharField(max_length=20, choices=task_status_choices, default="OPEN")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Mets:
+        unique_together = ('name', 'board')
+
+
+board_status_choices = (
+    ("1", "OPEN"),
+    ("2", "CLOSED")
+)
+
+class ProjectBoard(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.CharField(max_length=128)
+    tasks = models.ManyToManyField(User, blank=True, related_name="tasks_list")
+    status = models.CharField(max_length=20, choices=board_status_choices, default="OPEN")
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
