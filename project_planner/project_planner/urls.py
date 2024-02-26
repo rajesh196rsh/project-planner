eab3998754d9a3b32ebd7baa3b97f2ff52af1aea
rajesh196rsh <rajesh196rsh@gmail.com>
2024-02-27 00:42:41 +0530

    urls updated

--- project_planner/project_planner/urls.py
+++ project_planner/project_planner/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('planner/', include('base_board.urls')),
+    path('planner/', include('base_board.urls')),
 ]
