e8e53b01cc2cfc7ab52d9db5f331d3a4af7f891a
rajesh196rsh <rajesh196rsh@gmail.com>
2024-02-27 00:40:29 +0530

    user base updated

--- project_planner/base_board/user_base.py
+++ project_planner/base_board/user_base.py
@@ -1,3 +1,5 @@
+import json
+from .models import User, TeamUsersMapping, Team
 class UserBase:
     """
     Base interface implementation for API's to manage users.
@@ -18,7 +20,19 @@ class UserBase:
             * name can be max 64 characters
             * display name can be max 64 characters
         """
-        pass
+        data = json.loads(request)
+
+        if len(data["name"]) > 64:
+            raise ValueError("User name cannot exceed 64 characters")
+        if len(data["display_name"]) > 64:
+            raise ValueError("Display name cannot exceed 64 characters")
+
+        user_obj = User.objects.create(name=data["name"], display_name=data["display_name"])
+        res = {
+            "id": user_obj.id
+        }
+        res = json.dumps(res)
+        return res
 
     # list all users
     def list_users(self) -> str:
@@ -32,7 +46,19 @@ class UserBase:
           }
         ]
         """
-        pass
+        users_objs = User.objects.all(active=True)
+
+        res = []
+        for a_user in users_objs:
+            user = {
+              "name" : a_user.name,
+              "display_name" : a_user.display_name,
+              "creation_time" : a_user.created_at
+            }
+            res.append(user)
+
+        res = json.dumps(res)
+        return res
 
     # describe user
     def describe_user(self, request: str) -> str:
@@ -51,7 +77,19 @@ class UserBase:
         }
 
         """
-        pass
+        data = json.loads(request)
+
+        user_obj = User.objects.filter(id=data["user_id"], active=True)
+        if user_obj:
+            res = {
+                "name": user_obj[0].name,
+                "display_name": user_obj[0].display_name,
+                "creation_time": user_obj[0].created_at
+            }
+            res = json.dumps(res)
+        else:
+            res = "User does not exists"
+        return res
 
     # update user
     def update_user(self, request: str) -> str:
@@ -72,7 +110,15 @@ class UserBase:
             * name can be max 64 characters
             * display name can be max 128 characters
         """
-        pass
+        data = json.loads(request)
+
+        display_name = data.get("display_name")
+        if display_name:
+            if len(display_name) > 64:
+              raise ValueError("Display name cannot exceed 64 characters")
+            User.objects.update(display_name=display_name)
+
+        return "updated"
 
     def get_user_teams(self, request: str) -> str:
         """
@@ -90,5 +136,27 @@ class UserBase:
           }
         ]
         """
-        pass
+        data = json.loads(request)
 
+        user_id = data["id"]
+        user_teams = TeamUsersMapping.objects.filter(user_id=user_id, active=True)
+
+        user_teams_creation_map = {}
+        team_ids = []
+        for a_user_team in user_teams:
+            team_ids.append(a_user_team.team_id)
+            user_teams_creation_map[a_user_team.team_id] = a_user_team.created_at
+
+        teams = Team.objects.filter(id__in=team_ids)
+
+        res = []
+        for a_team in teams:
+            res.append(
+                {
+                    "name": a_team.name,
+                    "description": a_team.description,
+                    "creation_time": user_teams_creation_map[a_team.id]
+                }
+            )
+        res = json.dumps(res)
+        return res
